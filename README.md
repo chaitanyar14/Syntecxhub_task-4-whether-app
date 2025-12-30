9cc6a4e592db897ae5561eca146581b3ae9ed9a3
chaitanyar14 <chaitanyaraut149@gmail.com>
2025-12-30 13:40:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+🌦️ Weather App
+
+A modern and responsive Weather Application built using HTML, CSS, and JavaScript that fetches real-time weather data using the OpenWeatherMap API. This project is developed as part of the Syntecxhub Front-End Internship Task.
+
+
+
+🚀 Features
+- Search weather by city name
+- Displays temperature in Celsius
+- Shows humidity and wind speed
+- Dynamic weather icons
+- Loading animation while fetching data
+- Fully responsive and user-friendly UI
+- API integration using async/await
+
+
+
+🛠️ Technologies Used
+- HTML5 – Structure
+- CSS3 – Styling and responsive design
+- JavaScript (ES6) – Logic and API handling
+- OpenWeatherMap API – Live weather data
---
+++ script.js
@@ -0,0 +1,43 @@
+const apiKey = "35b3f17523bdad8d6ee38ab50b7e979e";
+
+async function getWeather() {
+    const city = document.getElementById("cityInput").value.trim();
+    const loader = document.getElementById("loader");
+    const weatherInfo = document.getElementById("weatherInfo");
+
+    if (city === "") {
+        alert("Please enter city name");
+        return;
+    }
+
+    loader.style.display = "block";
+    weatherInfo.style.display = "none";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+
+        const data = await response.json();
+
+        document.getElementById("city").innerText = data.name;
+        document.getElementById("temp").innerText = `${Math.round(data.main.temp)}°C`;
+        document.getElementById("humidity").innerText =
+            `💧 Humidity: ${data.main.humidity}%`;
+        document.getElementById("wind").innerText =
+            `🌬️ Wind: ${data.wind.speed} km/h`;
+        document.getElementById("icon").src =
+            `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        loader.style.display = "none";
+        weatherInfo.style.display = "block";
+
+    } catch (error) {
+        loader.style.display = "none";
+        alert("City not found or API key issue");
+    }
+}
